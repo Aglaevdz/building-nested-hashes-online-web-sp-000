bae1d50854baa2e04816c58852375ad5fd266b77
Aglae Verduzco <aglae.cv@gmail.com>
2018-12-26 01:03:28 +0000

    Automatically backed up by Learn

--- lib/bonus.rb
+++ lib/bonus.rb
@@ -20,11 +20,8 @@ def bonus
    }
   }
 
-  def epic_tragedy
     epic_tragedy[:montague][:hero][:status] = "dead"
     epic_tragedy[:capulet][:heorine][:status] = "dead"
-  end
-
 
 
   #Don't touch the following line! The `bonus` method must return our newly modified epic tragedy hash
